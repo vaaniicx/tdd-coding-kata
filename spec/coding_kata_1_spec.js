f7c409963d47cd9ea5dc07949d73bb2cc330add2
vaaniicx <vanessakausl@gmail.com>
2024-04-30 23:20:02 +0200

    Add todo for solving Coding Kata 1

---
+++ spec/coding_kata_1_spec.js
@@ -0,0 +1,6 @@
+/**
+ * TODO:
+ * - RED: Write test cases without implementing functional code
+ * - GREEN: Write functional code to pass the tests
+ * - REFACTOR: Refactor code if neccessary
+ */
---
+++ src/coding_kata_1.js
@@ -0,0 +1,20 @@
+/**
+ * Self Study 1 – TDD Coding Kata “Help ALF“
+ * 
+ * Late last night in the Tanner household, ALF was repairing his spaceship so he might get back to Melmac. 
+ * Unfortunately for him, he forgot to put on the parking brake, and the spaceship took off during repair. 
+ * Now it's hovering in space. ALF has the technology to bring the spaceship home if he can lock on to its location.
+ * 
+ * The map will be given in the form of a string with \n separating new lines. 
+ * The bottom left of the map is [0, 0]. X is ALF's spaceship.
+ * 
+ * In this example:
+ * findSpaceship(map) => [7, 2]
+ * If you cannot find the spaceship, the result should be "Spaceship lost forever."
+ */
+
+/**
+ * TODO:
+ * - Write function findSpaceship(map) -> returns Coordinates of found ship (x,y)
+ * - Return "Spaceship lost forever." if the ship cannot be found
+ */
